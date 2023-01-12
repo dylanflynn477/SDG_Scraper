b49e983793d9fb7f98e8602349a8bbd15162d5c2
dylanflynn477 <dylanflynn477@gmail.com>
2023-01-12 10:58:38 -0500

    saving progress

--- scraper.py
+++ scraper.py
@@ -2,22 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
+global formattedjournal
+formattedjournal = ""
+
 csv = pd.read_csv("journals.csv")
 
 journal_list = []
 journal_list = csv['Journal'].tolist()
 
 print(journal_list)
-class Scraper():
-    def formatjournal(journal):
-        # Change journal format to add %20 in between each space
-        formattedjournal = str(journal).replace(" ", "%20")
-        return formattedjournal
-    def query():
-        url = "https://sju.primo.exlibrisgroup.com/discovery/search?query=any,contains," + formattedjournal + "&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&offset=0"
-        page = requests.get(url)
+def formatjournal(journal):
+    # Change journal format to add %20 in between each space
+    formattedjournal = str(journal).replace(" ", "%20")
+    return formattedjournal
+def query():
+    url = "https://sju.primo.exlibrisgroup.com/discovery/search?query=any,contains," + formattedjournal + "&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&offset=0"
+    page = requests.get(url)
 
-s = Scraper()
 for i in range(len(journal_list)):
-    s.formatjournal(journal_list[i])
-    print(formattedjournal)
+    formatjournal(journal_list[i])
+    query()
